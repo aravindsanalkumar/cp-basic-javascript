b5bb67fc45ca06fc05f81a94fed9136b33a1168c
aravind <aravindsanalkumar@gmail.com>
2024-08-26 13:36:51 +0530

    filter method learned

--- hi_func.js
+++ hi_func.js
@@ -1,6 +1,5 @@
 let arr = [1,2,3,4,5];
 
-
 let result = arr.map((number) => number * 2);
 console.log(result);
 
@@ -14,4 +13,25 @@ const filter = (numbers,greaterThan) => {
 }
 
 console.log(filter(arr,2));
+const actors = [
+    {name:'johnny',netWorth:200000},
+    {name:'amber',netWorth:2000},
+    {name:'mattdemon',netWorth:170000},
+    {name:'dicaprio',netWorth:500000},
+    {name:'brad pitt',netWorth:300000},
+];
+
+console.log(actors.filter(actor => actor.netWorth > 200000));
+
+let receivedData = actors.filter(actor => actor.netWorth > 200000)
+let objData = JSON.stringify(receivedData);
+console.log(objData);
+
+let names = receivedData.map(actor => actor.name).join(', ')
+
+let playground = document.querySelector('.playground');
+
+playground.innerHTML = `<h1>${names}</h1>`;
+
+
 
